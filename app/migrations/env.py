a4f7eb7143f208e6f87ea1d70dc7cc2d4ef311f1
Sene4kaa <arsenyosychenko@yandex.ru>
2024-03-15 11:27:27 +0300

    New env

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -15,7 +15,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", DATABASE_URL.replace("postgres", "postgresql", 1))
+config.set_main_option("sqlalchemy.url", DATABASE_URL.replace("postgres", "postgresql+psycopg", 1))
 
 # add your model's MetaData object here
 # for 'autogenerate' support
