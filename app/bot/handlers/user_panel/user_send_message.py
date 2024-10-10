2dcfb96fd75d966233489729ae1e9dc4d91dad30
288346 <mr.56y@mail.ru>
2024-10-10 20:03:28 +0300

    Расписание занятий + контакты

--- app/bot/handlers/user_panel/user_send_message.py
+++ app/bot/handlers/user_panel/user_send_message.py
@@ -11,9 +11,9 @@ router = Router()
 @router.callback_query(F.data == "Connect")
 async def send_message(callback: CallbackQuery):
     
-    chat_link = hlink("Чат", "https://t.me/+YxoRDu70TpVmYzVi")
+    chat_link = hlink("Чат", "https://t.me/+bu3yOKgpEFpiYjI6")
     await callback.message.edit_text(
-        text=f"Контакты:\n\n{chat_link} - ссылка на чат\neabezyzvestnykh@itmo.ru - почта\n\nПо вопросам работы бота:\nadosychenko@itmo.ru", 
+        text=f"Контакты:\n\n{chat_link} - ссылка на чат\nmentors@itmo.ru - почта\n\nПо вопросам работы бота:\nvdroy@itmo.ru",
         reply_markup=get_back_to_user_menu_kb(),
         disable_web_page_preview=True
     )
